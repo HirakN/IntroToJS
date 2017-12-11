a79a3b6c2f867e51df11da8848956f8eb0eabc4d
Tech-A43 <tech-a43@Tech-A43s-MacBook-Air.local>
2017-12-11 14:58:29 +0000

    started calc proj

--- js-control-flow/app.js
+++ js-control-flow/app.js
@@ -1,7 +1,7 @@
 // A simple everyday if
 
 // Even empty objects are truthy
-var theThingWotIsTrue = [];
+var theThingWotIsTrue = false;
 
 if (theThingWotIsTrue) {
 	alert("dawg");
@@ -10,10 +10,37 @@ if (theThingWotIsTrue) {
 // In one line
 if (theThingWotIsTrue) alert("dawg");
 
-var dolla = 40;
+// Comparisons (>, <, >=, <=, ==, ===)
 
-if (dolla == 10) {
-	console.log("another G and T thank you sir.");
+// If/else
+var dolla = parseFloat(prompt("How much money on ya?"));
+var drink = parseInt(prompt("How many drinks have you had?"));
+var crispPrice = parseFloat(prompt("How much do the crisps cost?"));
+var crisp = parseInt(prompt("How many crisps have you already had?"));
+
+if ((dolla > 10) && (drink < 3)) {
+    drink++;
+    console.log("You have had " + drink + " drinks");
+} else if ((dolla > 5) || (crispPrice === 0)) {
+    crisp++;
+    console.log("You just bought crisps, you now have " + crisp);
+} else {
+    console.log("I'm going home!");
 }
 
-// Comparisons (>, <, >=, <=, ==, ===)
+// Switch statement
+var name = "Ollie";
+
+switch(name) {
+	case "Markson": 
+    	console.log("Good evening Mr Aigbodi");
+		break;
+	case "Stephen":
+		console.log("Salutations Mr Reid");
+		break;
+	case "Lexie": 
+    	console.log("Afternoon chief!");
+    	break;
+	default:
+    	console.log("yo");
+}

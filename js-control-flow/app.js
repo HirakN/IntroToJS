16f4831d3d833024464982f62389d6f92fa7b94b
Tech-A43 <tech-a43@Tech-A43s-MacBook-Air.local>
2017-12-11 13:32:28 +0000

    just finished control flow

---
+++ js-control-flow/app.js
@@ -0,0 +1,19 @@
+// A simple everyday if
+
+// Even empty objects are truthy
+var theThingWotIsTrue = [];
+
+if (theThingWotIsTrue) {
+	alert("dawg");
+}
+
+// In one line
+if (theThingWotIsTrue) alert("dawg");
+
+var dolla = 40;
+
+if (dolla == 10) {
+	console.log("another G and T thank you sir.");
+}
+
+// Comparisons (>, <, >=, <=, ==, ===)
